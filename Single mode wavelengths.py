0476c9ca53462862a6bc2853cccdb11ddd3ad7ba
thelimelines <brodierolph1@gmail.com>
2023-08-23 16:11:53 +0100

    Added max hole size calculation

--- Single mode wavelengths.py
+++ Single mode wavelengths.py
@@ -10,19 +10,20 @@ def mode_wavelength(f, a, m, n):
     
     # Cut-off wave numbers for the modes using Bessel function zeros
     kc_mn = jn_zeros(m, n)[-1] / a
-    
+    lambdac_mn = jn_zeros(m, n)[-1] / k
+
     # Propagation constants for each mode
     beta_mn = np.sqrt(k**2 - kc_mn**2)
     
     # Calculate the wavelength
     mode_lambda = 2 * np.pi / np.abs(beta_mn)
     
-    result_string = f"The wavelength for LP{m}{n} is {mode_lambda:.6f} m"
+    result_string = f"The wavelength for LP{m}{n} is {mode_lambda:.6f} m with a cutoff radius of {lambdac_mn*1000} mm"
     return result_string
 
 # Provided values
 f = 170e9  # 170 GHz in Hz
-a = 25e-3  # 50 mm diameter in meters
+a = 25E-3  # 50 mm diameter in meters
 m, n = 4, 4
 
 # Calculate and print the beat wavelength for LP01 and LP11
